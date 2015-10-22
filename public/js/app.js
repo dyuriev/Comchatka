7d0c964041d1e4754b3eb9a1067600f076dac454
Dmitriy Yuriev <coolkid00@gmail.com>
2015-10-23 02:50:37 +0300

    Interim update

--- public/js/app.js
+++ public/js/app.js
@@ -2,12 +2,18 @@
     $(function() {
         var socket = io(),
             $input = $('.msg-input'),
-            $form = $('.form-input');
+            $form = $('.form-input'),
+            $scrollTo = $('.scroll-to');
 
         $form.submit(function () {
-            socket.emit('USER_MESSAGE', $input.val());
-            socket.emit('PRIVATE_MESSAGE', 'CoolKid', $input.val());
-            $input.val('');
+            var message = $input.val().trim();
+
+            if (message) {
+                socket.emit('USER_MESSAGE', $input.val());
+                socket.emit('PRIVATE_MESSAGE', 'CoolKid', $input.val());
+                $input.val('');
+            }
+
             return false;
         });
 
@@ -15,6 +21,7 @@
             console.log(msg);
             $('.div-chat-messages').append('<div class="message-line"><div class="user-avatar"><img src="img/avatars/coolkid.jpg" class="img-user-avatar"></div>' +
                 '<div class="user-message-cell clearfix"><div class="user-message"><p class="message-content">' + msg + '</p><div class="ribbon"></div></div></div></div>');
+            $('.div-chat-scroll').scrollTo($scrollTo, 500);
         });
 
         socket.on('PRIVATE_MESSAGE', function (msg) {
