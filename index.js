d22b0bb79e0fd2a35e52bf46951bbc2c2970721b
Dmitriy Yuriev <coolkid00@gmail.com>
2017-10-17 20:29:41 +0400

    Interim

--- index.js
+++ index.js
@@ -5,12 +5,52 @@
 
 var express = require('express');
 var app = express();
+
+
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+http.listen(5000, function() {
+    console.log('listening on *:5000');
+});
+
+
+
 app.use(express.static(__dirname + '/public'));
 
-io.on('connection', function(socket){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+io.on('connection', function(socket) {
     //console.log(socket.client);
     console.log(socket.request.headers['user-agent']);
     console.log(socket.id);
@@ -18,21 +58,17 @@ io.on('connection', function(socket){
     //socket.broadcast.emit('SYSTEM_MESSAGE', 'В чат входит новый пользователь'); //всем, кроме самого юзера
     //socket.emit('SYSTEM_MESSAGE', 'Вы присоединились в разговору');  //самому юзеру
     io.emit('SYSTEM_MESSAGE', 'В чат входит новый пользователь'); //всем
-    
-    socket.on('USER_MESSAGE', function(msg){
+
+    socket.on('USER_MESSAGE', function(msg) {
         console.log('message: ' + msg);
         io.emit('USER_MESSAGE', msg);
     });
 
-    socket.on('PRIVATE_MESSAGE', function (from, msg) {
+    socket.on('PRIVATE_MESSAGE', function(from, msg) {
         console.log('I received a private message by ', from, ' saying ', msg);
     });
 
-    socket.on('disconnect', function(){
+    socket.on('disconnect', function() {
         socket.broadcast.emit('SYSTEM_MESSAGE', 'Пользователь покидает чат'); //всем, кроме самого юзера
     });
-});
-
-http.listen(5000, function(){
-  console.log('listening on *:5000');
 });
